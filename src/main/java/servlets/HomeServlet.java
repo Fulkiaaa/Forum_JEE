c3ee86912651e700c636618018c2a6d451058223
Laine William <laine.william01@gmail.com>
2025-02-09 13:49:11 +0100

    update

--- src/main/java/servlets/HomeServlet.java
+++ src/main/java/servlets/HomeServlet.java
@@ -15,35 +15,46 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class HomeServlet extends HttpServlet {
 	@Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        System.out.println("HomeServlet exécuté !");
-
         List<Subject> recentSubjects = new ArrayList<>();
+        
         try (Connection conn = DBConnection.getConnection()) {
-            String query = "SELECT * FROM sujets ORDER BY date_creation DESC LIMIT 5";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
+        
+        	String query = "SELECT * FROM sujets ORDER BY date_creation DESC LIMIT 5";
+            
+        	PreparedStatement stmt = conn.prepareStatement(query);
+            
+        	ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                Subject subject = new Subject(
-                        rs.getInt("id_sujet"),
+                
+            	Subject subject = new Subject(
+            
+                		rs.getInt("id_sujet"),
                         rs.getString("titre_sujet"),
                         rs.getString("contenu_sujet"),
                         rs.getInt("id_categorie"),
                         rs.getInt("id_utilisateur"),
                         rs.getTimestamp("date_creation")
                 );
-                recentSubjects.add(subject);
-                System.out.println("Sujet trouvé : " + subject.getTitle());
+                
+            	recentSubjects.add(subject);
+                
+            	System.out.println("Sujet trouvé : " + subject.getTitle());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } 
+        
+        catch (Exception e) {
+        
+        	e.printStackTrace();
         }
 
         System.out.println("Nombre de sujets récupérés : " + recentSubjects.size());
+        
         request.setAttribute("recentSubjects", recentSubjects);
+        
         request.getRequestDispatcher("/home.jsp").forward(request, response);
     }
 }
